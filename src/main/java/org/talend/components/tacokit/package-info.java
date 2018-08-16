6ca2b6e3de8395e524e7334de65c2f37f19f0216
Talend Component Kit Starter WebApp <tacokit@talend.com>
2018-08-16 08:00:40 +0000

    Importing Talend Component Project from the Starter application

---
+++ src/main/java/org/talend/components/tacokit/package-info.java
@@ -0,0 +1,9 @@
+// this tells the framework in which family (group of components) and categories (UI grouping)
+// the components in the nested packages belong to
+@Components(family = "Tacokit", categories = "Misc")
+@Icon(value = Icon.IconType.CUSTOM, custom = "Tacokit")
+package org.talend.components.tacokit;
+
+import org.talend.sdk.component.api.component.Components;
+import org.talend.sdk.component.api.component.Icon;
+
---
+++ src/main/java/org/talend/components/tacokit/service/Tacokit_componentService.java
@@ -0,0 +1,10 @@
+package org.talend.components.tacokit.service;
+
+import org.talend.sdk.component.api.service.Service;
+
+@Service
+public class Tacokit_componentService {
+
+    // you can put logic here you can reuse in components
+
+}
